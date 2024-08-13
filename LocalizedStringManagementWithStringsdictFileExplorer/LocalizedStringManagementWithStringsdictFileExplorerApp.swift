8bd30dd6d3891802cb22d6509d7e41ae3db73c9f
Kennedy Karimi on Mac <kennedymugera@gmail.com>
2024-08-13 17:04:27 +0300

    Initial Commit

---
+++ LocalizedStringManagementWithStringsdictFileExplorer/LocalizedStringManagementWithStringsdictFileExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalizedStringManagementWithStringsdictFileExplorerApp.swift
+//  LocalizedStringManagementWithStringsdictFileExplorer
+//
+//  Created by Kennedy Karimi on 13/08/2024.
+//
+
+import SwiftUI
+
+@main
+struct LocalizedStringManagementWithStringsdictFileExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
